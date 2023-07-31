a807eefba8d52b28167c091c77866fa79a20e3f7
moez <moez.benselem@smarteo.tn>
2023-07-31 14:55:02 +0100

    update ui

--- lib/screens/statistics/statistics_screen.dart
+++ lib/screens/statistics/statistics_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: must_be_immutable
+
 import 'package:flutter/material.dart';
 import 'package:hospital_app/constants/colors.dart';
 import 'package:hospital_app/custom_widgets/custom_app_bar.dart';
